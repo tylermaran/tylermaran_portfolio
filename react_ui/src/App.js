5bf0c0bf0f341b836625f407b913012cc63832c1
tyler.maran <tyler.maran@gmail.com>
2019-09-30 22:06:34 +0200

    initialize with create react app - cleaned up boilerplate

---
+++ react_ui/src/App.js
@@ -0,0 +1,18 @@
+// Importing Dependencies
+import React from 'react';
+
+// Importing Components
+import Header from './components/Header';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Header/>
+
+      Yup. It's react.
+
+    </div>
+  );
+}
+
+export default App;
---
+++ react_ui/src/components/Header.jsx
@@ -0,0 +1,16 @@
+// Importing Dependencies
+import React from 'react';
+
+// Importing Styling
+import './Header.css';
+
+
+const Header = () => {
+    return (
+        <div className="header">
+            This is a header
+        </div>
+    )
+}
+
+export default Header;
