4d9555ae6df90b5e2523d4855e005eafb62b8e6d
tyler.maran <tyler.maran@gmail.com>
2019-10-01 21:58:05 +0200

    added react router

--- react_ui/src/App.js
+++ react_ui/src/App.js
@@ -1,18 +1,21 @@
 // Importing Dependencies
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 // Importing Components
-import Header from './components/Header';
+import Landing from './pages/Landing';
+
 
 const App = () => {
-  return (
-    <div className="App">
-      <Header/>
-
-      Yup. It's react.
-
-    </div>
-  );
+	return (
+		<div className="App">
+			<Router>
+				<Switch>
+					<Route exact path='/' component={Landing}/>
+				</Switch>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
---
+++ react_ui/src/pages/Landing.jsx
@@ -0,0 +1,26 @@
+// Importing Dependencies
+import React from 'react';
+
+// Importing Components
+// import Header from '../components/Header'
+
+// Importing Styling
+import './Landing.css';
+
+const Landing = () => {
+
+    return (
+        <div className="landing">
+
+            <p>3D Visualization</p>
+            <p>Front End design</p>
+            <p>Full Stack applications</p>
+            <p>Serverless</p>
+            <p>Favorite Projects</p>
+
+        </div>
+    )
+
+}
+
+export default Landing;
