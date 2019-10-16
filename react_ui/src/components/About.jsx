9935da8b88a08e4d0792662cd9695c27058e07c2
tyler.maran <tyler.maran@gmail.com>
2019-10-15 23:38:44 -0700

    added featured / all switcher & pulling from JSON

--- react_ui/src/components/About.jsx
+++ react_ui/src/components/About.jsx
@@ -8,9 +8,10 @@ const About = (props) => {
 
     return (
         <div className='about'>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias minima recusandae nulla dolorem aut nemo iure asperiores pariatur consequatur eius provident, praesentium quidem itaque quaerat saepe, nobis perferendis, atque quas!
-            Perferendis quis maxime reprehenderit quod laudantium nostrum deserunt doloribus odit rem sunt similique exercitationem iste, nulla quibusdam quo unde? Praesentium illum consectetur eius possimus sint, ut ratione debitis distinctio voluptates.
-            Ab, reiciendis impedit! Pariatur, ullam neque dicta iusto in adipisci! Distinctio fuga deserunt dolore suscipit quae nihil saepe totam molestiae. Libero nemo voluptatum ullam excepturi similique odit culpa. Aperiam, mollitia.</p>
+            <p>Welcome to tylermaran.com! Featuring all things Tyler.</p>
+            <p>I live and work in San Francisco as a web developer, and hobbiest builder of just about anything. 
+                Lately, I have been heavily involved in 3D design . I received a BS in Economics, and began my career in M&amp;A Banking, before taking on additonal education in CS. </p>
+            <p>I am the Treasurer of <a href="http://www.noisebridge.net" target="new">NoiseBridge Hackerspace</a>, a 501(c)(3) non profit facility that provides tools, equipment, and classes on everything from Network Security to Sewing</p>
         </div>
     );
 
--- react_ui/src/pages/Landing.jsx
+++ react_ui/src/pages/Landing.jsx
@@ -1,5 +1,5 @@
 // Importing Dependencies
-import React from 'react';
+import React, {useState} from 'react';
 
 // Importing Components
 import Header from '../components/Header'
@@ -10,6 +10,8 @@ import About from '../components/About';
 // Importing Styling
 import './Landing.css';
 
+import projectData from '../projects.json';
+
 import izac from '../images/izac_front.png';
 import club from '../images/club_map.png';
 import outspoke from '../images/outspoke.png';
@@ -19,47 +21,92 @@ import phone from '../images/club_mobile.png';
 
 const Landing = () => {
 
+    const [featured, setFeatured] = useState(true);
+
+    const map_projects = (project) => {
+        return (
+            <div className="project_container" key={project.name}>
+                <Project image={project.desktop_image} phone_image={project.mobile_image}/>
+            </div>
+        );
+    }
+
+    let all_projects = projectData.map(map_projects);
+
+    let projects = (
+        <>
+            <h3 className="sub_title">
+                Full Stack
+            </h3>
+            <div className="project_container">
+                <Project image={cadl} phone_image={phone}/>
+            </div>
+            <div className="project_container">
+                <Project image={club} phone_image={phone}/>
+            </div>
+            
+            <h3 className="sub_title">
+                For fun
+            </h3>
+            <div className="project_container">
+                <Project image={izac} phone_image={phone}/>
+            </div>
+            <div className="project_container">
+                <Project image={club} phone_image={phone}/>
+            </div>
+
+            <h3 className="sub_title">
+                Front End
+            </h3>
+            <div className="project_container">
+                <Project image={outspoke} phone_image={phone}/>
+            </div>
+            <div className="project_container">
+                <Project image={dwayne} phone_image={phone}/>
+            </div>
+        </>
+    )
+
+    let all = (
+        <>
+            <h3 className="sub_title">
+                All Projects
+            </h3>
+            {all_projects}
+            
+        </>
+    )
+
+    let featured_highlight = (
+        <div className="switcher">
+            <div className="featured_button" onClick={() => setFeatured(true)} style={{backgroundColor: "rgb(30,30,30)"}}>
+                Featured
+            </div>
+            <div className="all_button" onClick={() => setFeatured(false)}>
+                All Projects
+            </div>
+        </div>
+    )
+
+    let all_highlight = (
+        <div className="switcher">
+            <div className="featured_button" onClick={() => setFeatured(true)}>
+                Featured
+            </div>
+            <div className="all_button" onClick={() => setFeatured(false)} style={{backgroundColor: "rgb(30,30,30)"}}>
+                All Projects
+            </div>
+        </div>
+    )
+
     return (
         <div className="landing">
             <Header/>
             <About/>
-            {/* <div className="left"> */}
-                <h3 className="sub_title">
-                    Full Stack
-                </h3>
-                <div className="project_container">
-                    <Project image={cadl} phone_image={phone}/>
-                </div>
-                <div className="project_container">
-                    <Project image={club} phone_image={phone}/>
-                </div>
-                
-                <h3 className="sub_title">
-                    For fun
-                </h3>
-                <div className="project_container">
-                    <Project image={izac} phone_image={phone}/>
-                </div>
-                <div className="project_container">
-                    <Project image={club} phone_image={phone}/>
-                </div>
-
-            {/* </div> */}
-
-            {/* <div className="right"> */}
-                <h3 className="sub_title">
-                    Front End
-                </h3>
-                <div className="project_container">
-
-                <Project image={outspoke} phone_image={phone}/>
-                </div>
-                <div className="project_container">
-
-                <Project image={dwayne} phone_image={phone}/>
-                </div>
-
-            {/* </div> */}
+            <br/>
+            
+            {featured? featured_highlight: all_highlight}
+            {featured? projects: all}            
 
             <Footer/>
 
