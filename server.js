284c0105b1f950217f6271a6df084ccf58adab06
themanmaran <tyler.maran@gmail.com>
2019-10-28 12:16:05 -0700

    added the /*

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const port = process.env.PORT || 8000;
 const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('/', function(req, res) {
+app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 app.listen(port, () => {
